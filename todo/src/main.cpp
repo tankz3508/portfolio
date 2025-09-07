feb93234fff5bc721e173fd423b7d335d6d33103
tankz <iostankz@proton.me>
2025-09-07 18:26:46 +0200

    Create main.cpp

---
+++ todo/src/main.cpp
@@ -0,0 +1,17 @@
+#include "../include/todo.h"
+
+int main() {
+    // DUMMY SKELETON CODE
+    Todo todo1 { "Title", "Description", false };
+
+    todo1.setName("New Title");
+    todo1.setDescription("New Description");
+    todo1.setCompleted(true);
+
+    todo1.write("output.txt");
+    todo1.load("output.txt");
+
+    // todo1.clear("output.txt");
+
+    return 0;
+}
